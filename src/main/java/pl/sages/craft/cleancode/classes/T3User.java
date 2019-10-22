dd579aabae5860a9e1c6bb04709a81cac19e340b
Piotr Krogulec <krogulec.piotr@gmail.com>
2019-10-23 00:58:51 +0200

    adding task 3 demeter law

---
+++ src/main/java/pl/sages/craft/cleancode/classes/T3User.java
@@ -0,0 +1,47 @@
+package pl.sages.craft.cleancode.classes;
+
+import java.math.BigDecimal;
+
+/**
+ * @author krogulecp
+ */
+public class T3User {
+    private Account account;
+
+    private double discountedPlanPrice(String code){
+        Coupon coupon = new Coupon();
+        return coupon.discount(account.getPlan().getPrice());
+    }
+}
+
+
+class Account {
+    private Plan plan;
+
+    public Plan getPlan() {
+        return plan;
+    }
+
+    public void setPlan(Plan plan) {
+        this.plan = plan;
+    }
+}
+
+class Coupon {
+    double discount(BigDecimal price){
+        return 0;
+    }
+}
+
+class Plan {
+    private BigDecimal price;
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
+
