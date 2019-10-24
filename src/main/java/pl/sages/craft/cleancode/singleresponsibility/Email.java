796a58bbb07591f05b6623f395195b8515a00c65
Piotr Krogulec <krogulec.piotr@gmail.com>
2019-10-24 22:58:41 +0200

    adding single responsiobility starting point

---
+++ src/main/java/pl/sages/craft/cleancode/singleresponsibility/Email.java
@@ -0,0 +1,56 @@
+package pl.sages.craft.cleancode.singleresponsibility;
+
+/**
+ * @author krogulecp
+ */
+//TODO Refactor aby była zachowana Single Responsibility Principle
+class Email {
+    private String from;
+    private String to;
+    private String subject;
+    private String message;
+
+    public void send(){
+        System.out.println("sending email");
+    }
+
+    public void validate(){
+        if (message.contains("polityka")){
+            System.out.println("sending not permitted");
+        } else {
+            System.out.println("email valid");
+        }
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
