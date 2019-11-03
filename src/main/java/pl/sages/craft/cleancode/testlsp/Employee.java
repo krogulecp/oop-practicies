5018cde0c545206ea51c324fd3c5f53514438ca4
Piotr Krogulec <krogulec.piotr@gmail.com>
2019-11-03 20:36:37 +0100

    test LSP starting point

---
+++ src/main/java/pl/sages/craft/cleancode/testlsp/Employee.java
@@ -0,0 +1,21 @@
+package pl.sages.craft.cleancode.testlsp;
+
+/**
+ * @author krogulecp
+ */
+class Employee {
+    void takeSalary(){
+        //do something
+    }
+}
+
+class Volonteer extends Employee{
+    //TODO
+}
+
+class ClientClass{
+
+    public static void main(String[] args) {
+        //TODO
+    }
+}
