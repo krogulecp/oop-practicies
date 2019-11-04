db8ba98cf36cf63cb4aa02507efd7bb4938a30dc
Admin <piotrek@whatever.com>
2019-11-04 14:03:27 +0100

    testowa zmiana na branch

--- src/main/java/pl/sages/craft/cleancode/singleresponsibility/Email.java
+++ src/main/java/pl/sages/craft/cleancode/singleresponsibility/Email.java
@@ -5,7 +5,7 @@ package pl.sages.craft.cleancode.singleresponsibility;
  */
 //TODO Refactor aby była zachowana Single Responsibility Principle
 class Email {
-    private String from;//testowa
+    private String from;
     private String to;
     private String subject;
     private String message;
