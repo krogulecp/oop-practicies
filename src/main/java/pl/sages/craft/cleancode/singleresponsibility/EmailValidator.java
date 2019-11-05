6f041bc82a83221400efb7a2df87985946a63c5e
jacekwajszczuk <aaa>
2019-11-05 11:06:45 +0100

    aaa

--- src/main/java/pl/sages/craft/cleancode/singleresponsibility/EmailValidator.java
+++ src/main/java/pl/sages/craft/cleancode/singleresponsibility/EmailValidator.java
@@ -1,8 +1,9 @@
 package pl.sages.craft.cleancode.singleresponsibility;
 
 public class EmailValidator {
+    private Email email;
     public void validate(){
-        if (message.contains("polityka")){
+        if (email.getMessage.contains("polityka")){
             System.out.println("sending not permitted");
         } else {
             System.out.println("email valid");
