65168a03b37e5539797111b55c54fea02888a89c
Piotr Krogulec <krogulec.piotr@gmail.com>
2019-11-05 20:23:12 +0100

    prototype init

---
+++ src/main/java/pl/sages/craft/cleancode/designpatterns/prototype/Sheep.java
@@ -0,0 +1,12 @@
+package pl.sages.craft.cleancode.designpatterns.prototype;
+
+/**
+ * @author krogulecp
+ */
+abstract class Sheep {
+    private final String name;
+
+    protected Sheep(String name) {
+        this.name = name;
+    }
+}
