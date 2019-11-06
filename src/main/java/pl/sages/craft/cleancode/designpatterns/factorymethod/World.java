93f4cb5f1e77a10d075bad05b5e78562f32e06d4
Admin <piotrek@whatever.com>
2019-11-06 12:11:12 +0100

    Factory - nieskończone generowanie z wykorzystaniem Streamów

--- src/main/java/pl/sages/craft/cleancode/designpatterns/factorymethod/World.java
+++ src/main/java/pl/sages/craft/cleancode/designpatterns/factorymethod/World.java
@@ -1,5 +1,9 @@
 package pl.sages.craft.cleancode.designpatterns.factorymethod;
 
+import sun.java2d.opengl.WGLSurfaceData;
+
+import java.util.stream.IntStream;
+
 public class World {
     private EnemyFactory enemyFactory;
     public World(EnemyFactory enemyFactory){
@@ -9,4 +13,15 @@ public class World {
     public Enemy createNewEnemy(){
         return enemyFactory.rise();
     }
+
+    void createWorld(){
+        IntStream.iterate(0, i -> i+1)
+                .forEach(i -> System.out.println(enemyFactory.rise().getClass().getSimpleName()));
+    }
+
+    public static void main(String[] args) {
+        World world = new World(new RandomEnemyFactory());
+        world.createWorld(); // nieskończone generowanie
+    }
+
 }
